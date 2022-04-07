d5f7433469968fa37ebea49d46f5cf95ecc6c451
anusha-thatichetla <tanusha2329@gmail.com>
2022-04-07 20:54:30 +0530

    commit

--- helloWorld.js
+++ helloWorld.js
@@ -1,15 +1,7 @@
 import { LightningElement,track} from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-  /*  name //undefined
-    age=23
-    fullname="salesforce troop"
-    showData =false
-    details ={
-        name:"dummy",
-        place:"nellore"
-        
-    } */
+ 
     userList=["A","B","C"] 
     fullname="zero to hero"
     title="lwc"
@@ -17,24 +9,5 @@ export default class HelloWorld extends LightningElement {
     {
         this.title=event.target.value
     }
-     address ={
-        city:'melbourne',
-        postcode:3008,
-        country:'australia'   
-    }
-    TrackHandler(event){
-        this.address={...this.address,"city":event.target.value}
-    } 
-/**getter example  **/
-    users = ["anu", "hanu", "mummy"]
-    num1 = 10
-    num2 = 20
-
-    get firstUser(){
-        return this.users[0].toUpperCase()
-    }
-    get multiply(){
-        return this.num1*this.num2
-    }
-
+    
 }
